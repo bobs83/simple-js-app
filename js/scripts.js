81d21169ad3d484a6370375dea9992d06e38d074
bobs83 <124786894+bobs83@users.noreply.github.com>
2023-08-26 18:30:21 +0200

    Practice working with variables

--- js/scripts.js
+++ js/scripts.js
@@ -5,5 +5,18 @@ let lastName = 'Singh';
 let myAge ='40';
 document.write(firstName, lastName, myAge);
 
-//const interestRate = 0,3
-//interestRate
+let interestRate = 0.3;
+let price = 1000;
+let interestTag = 'The interest is:';
+let interest = interestRate * price;
+document.write(interestTag, interest);
+
+//more difficult calculation 
+
+let BobbyFoodtag = 'Bobby favorite food is: ';
+let favoriteFood = 'pasta';
+let minTomatotag = 'The least amount of tomatoes in his pasta mix are:'
+let tomatoes = 100;
+let dubbleTomatoes = tomatoes * 2;
+let minTomatoes = (dubbleTomatoes * 5) - (tomatoes / 2);
+document.write(BobbyFoodtag, favoriteFood, minTomatotag, minTomatoes)
