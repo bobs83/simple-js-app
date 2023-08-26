5c9682c3a455e2baace7c35b089ed8f6cd9adb26
bobs83 <124786894+bobs83@users.noreply.github.com>
2023-08-26 17:31:18 +0200

    Updating of pathways for css and Js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 alert('Hello world');
+
+let firstName = 'Bobby';
+let lastName = 'Singh';
+let myAge ='40';
+document.write(firstName, lastName, myAge);
+
+//const interestRate = 0,3
+//interestRate
