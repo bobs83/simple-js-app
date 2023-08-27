3e2951aa3af3d3df28cf9b5b011127933f4dfd9e
bobs83 <124786894+bobs83@users.noreply.github.com>
2023-08-27 20:51:49 +0200

    Create four Pokémon objects

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+var pokemonList=[
+    
+    {
+        name: 'flareon',
+        types: ['fire']
+        species: 'lizard',
+        height: 0.6,
+        weight: 8.5,
+ 
+    },
+    {
+        name: 'flareon',
+        types: ['fire']
+        species: 'flame',
+        height: 0.9,
+        weight: 25 ,
+
+    },
+    {
+        name: 'pikachu',
+        types: ['electric']
+        species: 'mouse',
+        height: 0.4,
+        weight: 6,
+
+    },
+    {
+        name: 'psyduck',
+        types: ['Water']
+        species: 'Duck',
+        height: 0.8,
+        weight: 19.6,
+
+    },
+    {
+        name: 'mewtwo',
+        types: ['psychic']
+        species: 'genetic',
+        height: 2,
+        weight: 122,
+
+    }
+];
+console.log(pokemonList);
